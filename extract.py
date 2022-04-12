17657a56291c0fc4fb097c4b10c14e35f8f01f08
Ian Robinson <ianrobinson171@gmail.com>
2022-04-12 09:47:48 -0500

    working with player data

--- extract.py
+++ extract.py
@@ -1,16 +1,22 @@
+from unicodedata import name
 import creds
 import requests
 import pandas as pd
+from matplotlib import pyplot as plt
 
 headers = {
-    'authorization': creds.desktop_key,
+    'authorization': creds.my_key,
     'Accept': 'application/json',
 }
 
-response = requests.get('https://api.clashofclans.com/v1/players/%2380J99Y8Y', headers=headers)
-user_json = response.json()
-#print(user_json)
+response = requests.get('https://api.clashofclans.com/v1/players/%2382UQ2G0J', headers=headers)
+player_data = response.json()
+#print(player_data)
 
-df = pd.json_normalize(user_json)
-player_stats = df.loc[:, ['name', 'townHallLevel', 'attackWins', 'defenseWins']]
+df = pd.json_normalize(player_data)
+df.set_index('name', inplace=True)
+
+num_data = ['townHallLevel', 'attackWins', 'defenseWins', 'donations', 'donationsReceived', 'trophies']
+
+player_stats = df.loc[:, num_data]
 print(player_stats)
